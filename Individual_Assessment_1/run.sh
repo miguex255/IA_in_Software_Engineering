2e08611df44add5284db4cdfb5383de1a6b07cd7
Miguel Yanez Soffia <miguelantonioyanez@gmail.com>
2023-03-05 21:33:42 +0100

    Run files have been fixed

--- Individual_Assessment_1/run.sh
+++ Individual_Assessment_1/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 docker run --rm -p 8070:8070 --name grobid -d lfoppiano/grobid:0.7.2
 sleep 3 
-docker rm miguelynz/assessment_1
+docker rm assessment_1
 docker build -t miguelynz/assessment_1 .
-docker run --rm -it -v $(pwd)/templates/:/app/templates/ -v $(pwd)/static/:/app/static/ --name miguelynz/assessment_1 --network=host miguelynz/assessment_1
+docker run --rm -it -v $(pwd)/templates/:/app/templates/ -v $(pwd)/static/:/app/static/ --name assessment_1 --network=host miguelynz/assessment_1
 docker stop grobid 
-docker rm miguelynz/flask
+docker rm flask
 docker build -t miguelynz/flask -f flask/dockerfile .
-docker run --rm -it -p 8080:8080 --name miguelynz/flask miguelynz/flask
+docker run --rm -it -p 8080:8080 --name flask miguelynz/flask
